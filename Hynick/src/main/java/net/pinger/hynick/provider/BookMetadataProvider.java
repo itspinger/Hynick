c3d763e2e45ed2bd56121ea462831d3628198363
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-09-22 20:17:25 +0200

    Ensure BookMetadataProvider can be used only on classes

--- Hynick/src/main/java/net/pinger/hynick/provider/BookMetadataProvider.java
+++ Hynick/src/main/java/net/pinger/hynick/provider/BookMetadataProvider.java
@@ -25,6 +25,13 @@
 
 package net.pinger.hynick.provider;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface BookMetadataProvider {
 
     /**

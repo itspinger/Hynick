7143cbf6035e49ac8e83a6a1dfa4807519bab887
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-09-22 20:20:20 +0200

    Add slf4j-simple dependency

--- Hynick/src/main/java/net/pinger/hynick/HynickAPI.java
+++ Hynick/src/main/java/net/pinger/hynick/HynickAPI.java
@@ -23,6 +23,14 @@
 
 package net.pinger.hynick;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HynickAPI {
 
+    private static final Logger logger = LoggerFactory.getLogger("Hynick");
+
+    public static Logger getLogger() {
+        return logger;
+    }
 }
